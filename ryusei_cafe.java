6bad6cb95a925e0179c6af8f33626ded36b01469
lucas <lucasscpereira14@gmail.com>
2025-08-28 22:07:26 -0400

    add de funcoes e polimento

--- ryusei_cafe.java
+++ ryusei_cafe.java
@@ -15,6 +15,14 @@ class Usuario {
         this.assinatura = Assinatura;
     }
     
+    public void checarAssinatura () {
+        System.out.println("A assinatura desse usuário é nivel " + this.assinatura);
+    }
+
+    public void mudaAssinatura(char novaAssinatura) {
+        this.assinatura = novaAssinatura;
+    }
+    
 }
 
 class manga {
@@ -26,9 +34,10 @@ class manga {
     private int volume;
     private String localizacao;
     private int qtdVenda;
+    private int estoque;
     //private int qtdLeitura_manga; *como não vamos emprestar mangas, sugiro retirar isso
 
-    public void criarManga(String id, String Nome, String[] auto, String[] gen, String ser, int vol, String local, int qVen, int qLei){
+    public void criarManga(String id, String Nome, String[] auto, String[] gen, String ser, int vol, String local, int qVen, int estoque){
         this.id = id;
         this.nome = Nome;
         this.autores = auto;
@@ -37,32 +46,46 @@ class manga {
         this.volume = vol;
         this.localizacao = local;
         this.qtdVenda = qVen;
+        this.estoque = estoque;
         //this.qtdLeitura = qLei;
     }
+
   /*public void idAtualiza(String id){ 
         this.id = id;
     } não vamos precisar trocar o id de um item*/
     public void nomeAtualiza(String Nome){
         this.nome = Nome;
     }
+
     public void autoresAtualiza(String[] aut){
         this.autores = aut;
     }
+
     public void generosAtualiza(String[] gen){
         this.generos = gen;
     }
+
     public void serieAtualiza(String ser){
         this.serie = ser;
     }
+
     public void volumeAtualiza(int vol){
         this.volume = vol;
     }
+
     public void localizacaoAtualiza(String local){
         this.localizacao = local;
     }
+
     public void qtdVendaAtualiza(int qtd){
         this.qtdVenda += qtd;
+        this.estoque -= qtd;
     }
+
+    public void estoqueAtualiza (int mangas_novos) {
+        this.estoque += mangas_novos;
+    }
+    
     /*public void qtdLeituraAtualiza(int qtd){
         this.qtdLeitura += qtd;
     }*/
@@ -100,7 +123,7 @@ class pagamentos {
     }
 }
 
-public class Item_Menu {
+class Item_Menu {
     private String nome;
     private String ingredientes;
     private int qtdVenda;
@@ -131,3 +154,10 @@ public class Item_Menu {
 
 }
 
+
+//vai ser a nossa main
+public class ryusei_cafe {
+    public static void main (String[] args) {
+        
+    }
+}
